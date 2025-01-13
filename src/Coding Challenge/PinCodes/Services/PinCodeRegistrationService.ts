c5f2a85cec931f6a9f4779459cb826e3950784ad
cholakovit <spasvcholakov@gmail.com>
2025-01-13 12:31:54 +0200

    some cleaning

--- src/Coding Challenge/PinCodes/Services/PinCodeRegistrationService.ts
+++ src/Coding Challenge/PinCodes/Services/PinCodeRegistrationService.ts
@@ -4,6 +4,8 @@ import { DoorService } from "../../Doors/Services/DoorService";
 import { LogEnabled } from "../../../Boilerplate/Logging/LogEnabled";
 import { PinCodeRegistrationRepository } from "../Repositories/PinCodeRegistrationRepository";
 import { CacheResult, ClearCache } from "src/helper/decorators";
+import ApiError from "src/helper/ApiError";
+import { StatusCodes } from "http-status-codes";
 
 @Injectable()
 export class PinCodeRegistrationService extends LogEnabled {
@@ -17,7 +19,7 @@ export class PinCodeRegistrationService extends LogEnabled {
   /**
    * Gets all current registrations with caching.
    */
-  @CacheResult(120, 'user-registrations')
+  @CacheResult(120, "user-registrations")
   async getRegistrations() {
     // nothing to do here - just a little helper for you during testing
     return this.pinCodeRegistrationRepository.findAll();
@@ -52,20 +54,12 @@ export class PinCodeRegistrationService extends LogEnabled {
       `PIN code ${registration.pinCode} registered successfully for user: ${userId}`
     );
 
-    //return await this.doorService.getDoors();
     return {
       message: `PIN code ${registration.pinCode} registered successfully.`,
+      pinCode: registration.pinCode,
+      doorIds: registration.doorIds,
+      restrictions: registration.restrictions || [],
     };
-  }
-
-  private async validateDoors(doorIds: string[]) {
-    const availableDoors = await this.doorService.getDoors();
-    const invalidDoors = doorIds.filter(
-      (doorIds) => !availableDoors.some((door) => door.id === doorIds)
-    );
-    if (invalidDoors.length > 0) {
-      throw new Error(`Doors ${invalidDoors.join(", ")} are not available`);
-    }
   }
 
   /**
@@ -82,9 +76,7 @@ export class PinCodeRegistrationService extends LogEnabled {
         registration.pinCode
       );
     if (!existingRegistration) {
-      const errorMsg = `No registration found for PIN code: ${registration.pinCode}, user: ${userId}`;
-      this.logger.error(errorMsg);
-      throw new Error(errorMsg);
+      throw new ApiError(StatusCodes.BAD_REQUEST, `No registration found for PIN code: ${registration.pinCode}, user: ${userId}`);
     }
 
     await this.validateDoors(registration.doorIds);
@@ -100,11 +92,12 @@ export class PinCodeRegistrationService extends LogEnabled {
       updatedRegistration
     );
 
-    this.logger.info(
-      `PIN code ${registration.pinCode} updated successfully for user: ${userId}`
-    );
-    //return await this.doorService.getDoors();
-    return { message: "PIN code updated successfully." };
+    return {
+      message: `PIN code ${registration.pinCode} updated successfully for user: ${userId}`,
+      pinCode: registration.pinCode,
+      doorIds: registration.doorIds,
+      restrictions: registration.restrictions || [],
+    };
   }
 
   /**
@@ -114,22 +107,24 @@ export class PinCodeRegistrationService extends LogEnabled {
     const existingRegistration =
       await this.pinCodeRegistrationRepository.getRegistration(userId, pinCode);
     if (!existingRegistration) {
-      throw new Error(
-        `No registration found for PIN code: ${pinCode}, user: ${userId}`
-      );
+      throw new ApiError(StatusCodes.BAD_REQUEST, `No registration found for PIN code: ${pinCode}, user: ${userId}`);
     }
 
     await this.pinCodeRegistrationRepository.deletePicCodeRegistration(
       userId,
       pinCode
     );
-    ClearCache('user-registrations');
+    ClearCache("user-registrations");
 
     this.logger.info(
       `PIN code ${pinCode} revoked successfully for user: ${userId}`
     );
-    //return await this.doorService.getDoors();
-    return { message: "PIN code deleted successfully." };
+
+    return {
+      message: `PIN code ${pinCode} updated successfully for user: ${userId}`,
+      pinCode: pinCode,
+      userId: userId,
+    };
   }
 
   async validateAccess(
@@ -140,13 +135,8 @@ export class PinCodeRegistrationService extends LogEnabled {
   ): Promise<boolean> {
     const registration =
       await this.pinCodeRegistrationRepository.getRegistration(userId, pinCode);
-    if (!registration) {
-      return false;
-    }
-
-    if (!registration.doorIds.includes(doorId)) {
-      return false;
-    }
+    if (!registration) return false;
+    if (!registration.doorIds.includes(doorId)) return false;
 
     if (registration.restrictions) {
       for (const restriction of registration.restrictions) {
@@ -171,9 +161,22 @@ export class PinCodeRegistrationService extends LogEnabled {
     );
   }
 
-  async isPinCodeRegistered(userId: string, pinCode: string): Promise<boolean> {
-    const registration =
-      await this.pinCodeRegistrationRepository.getRegistration(userId, pinCode);
-    return !!registration;
+  // async isPinCodeRegistered(userId: string, pinCode: string): Promise<boolean> {
+  //   const registration =
+  //     await this.pinCodeRegistrationRepository.getRegistration(userId, pinCode);
+  //   return !!registration;
+  // }
+
+  private async validateDoors(doorIds: string[]) {
+    const availableDoors = await this.doorService.getDoors();
+    const invalidDoors = doorIds.filter(
+      (doorIds) => !availableDoors.some((door) => door.id === doorIds)
+    );
+    if (invalidDoors.length > 0) {
+      throw new ApiError(
+        StatusCodes.BAD_REQUEST,
+        `Doors ${invalidDoors.join(", ")} are not available`
+      );
+    }
   }
 }
